7df95a02a1632b687e0925e0db6e8c7ab01412e0
Tanyiqu <1953649096@qq.com>
2021-02-04 18:52:30 +0800

    简单使用

---
+++ 简单使用/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <quill-editor
+      id="quill-editor"
+      v-model="content"
+      :options="editorOption"
+      @change="onEditorChange($event)"
+    >
+    </quill-editor>
+  </div>
+</template>
+
+<script>
+import QuillEditor from 'vue-quill-editor'
+
+export default {
+  name: 'App',
+  data: function () {
+    return {
+      content: '',
+      editorOption: {}
+    }
+  },
+  methods: {
+    onEditorChange() {
+      console.log(this.content);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  width: 800px;
+  height: 500px;
+}
+
+#quill-editor {
+  width: 100%;
+  height: 100%;
+}
+</style>
